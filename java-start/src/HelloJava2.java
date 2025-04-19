b0d16191bc6e77f22f260beb6ee11c869a09ad55
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2025-04-19 21:26:24 +0900

    Add files via upload

---
+++ java-start/src/HelloJava2.java
@@ -0,0 +1,7 @@
+public class HelloJava2 { //클래스
+    public static void main(String[] args){ //메서드
+        System.out.println("hello java1"); //실행내용
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+    }
+}
